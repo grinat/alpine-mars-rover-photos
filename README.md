1f638f43593baec378532c40d06cbd57395886fe
grinat <paladin2012gnu@gmail.com>
2020-06-06 14:47:09 +0300

    feat: fetch and render rovers photos

---
+++ README.md
@@ -0,0 +1,4 @@
+# alpine-mars-rover-photos
+
+Fetch mars rovers photos from NASA and show on page.
+Written on [Alpine.js](https://github.com/alpinejs)
---
+++ script.js
@@ -0,0 +1,63 @@
+function photosList() {
+  return {
+    loading: false,
+    photos: [],
+    date: '2015-06-30',
+    rovers: ['curiosity', 'opportunity'],
+    /**
+     * @link https://github.com/chrisccerami/mars-photo-api
+     * @param rover
+     * @param date
+     * @returns {string}
+     */
+    getRoverPhotosUrl(rover, date) {
+      return `https://mars-photos.herokuapp.com/api/v1/rovers/${rover}/photos?earth_date=${date}`
+    },
+    async fetchData(date) {
+      try {
+        this.loading = true
+
+        const results = await Promise.all(
+          this.rovers.map(rover =>
+            fetch(this.getRoverPhotosUrl(rover, date))
+              .then(r => r.ok ? r.json() : Promise.reject(r.statusText || 'Smth wrong'))
+          )
+        )
+        this.photos = this.orderData(this.transformData(this.mergeData(results)))
+      } catch (e) {
+        alert(`Error: ${e.toString()}`)
+      } finally {
+        this.loading = false
+      }
+    },
+    /**
+     * [{"photos":[item1]},{"photos":[item2]}] -> [item1, item2]
+     * @param results
+     * @returns {*}
+     */
+    mergeData(results) {
+      return results.reduce((prev, curr) => {
+        prev = [...prev, ...curr.photos]
+        return prev
+      }, [])
+    },
+    transformData(photos) {
+      return photos.map(photo => {
+        const date = new Date(photo.earth_date)
+
+        let caption = ''
+        caption += `${photo.id} `
+        caption += `${photo.rover.name}(${photo.camera.full_name}) `
+        caption += `${date.toLocaleDateString()} `
+
+        return {
+          ...photo,
+          unixTime: +date,
+          caption,
+        }})
+    },
+    orderData(photos) {
+      return photos.sort((a, b) => a.id - b.id)
+    }
+  }
+}
